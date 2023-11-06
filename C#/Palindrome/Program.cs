a5ae791d5c43d628f224eb39a1158f37d09a7ab0
halilacari <halilacari@outlook.com>
2023-11-06 03:29:36 +0300

    Added Palindrome in C#

---
+++ C#/Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Enter a word : ");
+
+                string word = Console.ReadLine();
+                string reverse = "";
+ 
+                for (int i = word.Length -1 ; i>=0 ; i--)
+                {
+                    reverse += word[i];
+                }
+
+                if (reverse == word) { Console.WriteLine("Your word is a palindrome"); }
+                else { Console.WriteLine("Your word is not a palindrome"); }
+            
+        }
+    }
+}
